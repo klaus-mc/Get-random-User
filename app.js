ab016169927b5ee965cab43ca6857943c020475a
klaus-mc <klausarnaud@yahoo.com>
2021-10-13 01:18:07 +0200

    app has been modified

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app= Vue.createApp({
     data() {
         return{
             firstName: 'klaus',
-            lastName: 'Müller',
+            lastName: 'Mann',
             email: 'john@gmail.com',
             gender: 'male',
             picture: 'https://randomuser.me/api/portraits/men/10.jpg',
@@ -15,14 +15,11 @@ const app= Vue.createApp({
         async getUser() {
             const res = await fetch('https://randomuser.me/api')
             const {results} = await res.json()
-
-
             this.firstName = results[0].name.first
             this.lastName = results[0].name.last
             this.email = results[0].email 
             this.gender = results[0].gender
-            this.picture = results[0].picture.large
-
+            this.picture = 'https://avatars.dicebear.com/api/'+results[0].gender+'/'+ Math.floor(Math.random() * 5000)+'.svg'
         },
 
     },  
